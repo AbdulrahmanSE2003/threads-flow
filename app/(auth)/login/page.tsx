4ae4c1b99bbce0cc1e55fc89817d44e4461c448a
AbdulrahmanSE2003 <abdulrahman.saad2303@gmail.com>
2026-03-24 09:27:58 +0200

    add

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import LoginForm from "./_components/LoginForm";
+import { AtSign } from "lucide-react";
 
 const LoginPage = async () => {
   return (
-    <div className="min-h-screen flex items-center justify-center text-foreground bg-background">
+    <div className="min-h-screen w-full flex items-center justify-center text-foreground bg-background">
       <div className="absolute max-md:hidden -left-20 right-0 -top-24 h-100">
         <Image
           src={"https://static.cdninstagram.com/rsrc.php/yC/r/JlaY6JCPfe-.avif"}
@@ -11,7 +12,17 @@ const LoginPage = async () => {
           fill
         />
       </div>
+
       <div className="w-full max-w-2xl px-6 z-10 md:mt-20">
+        <div className="flex flex-col items-center">
+          <div className="bg-black dark:bg-white p-3 rounded-2xl mb-4">
+            <AtSign
+              size={40}
+              className="text-white dark:text-black"
+              strokeWidth={2.5}
+            />
+          </div>
+        </div>
         <div className="text-center mb-8">
           <h1 className="text-3xl font-bold tracking-tight text-black dark:text-white">
             Welcome back
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -2,13 +2,14 @@ import { getSession } from "@/lib/auth/session";
 import { redirect } from "next/navigation";
 import RegisterForm from "./_components/RegisterForm";
 import Image from "next/image";
+import { AtSign } from "lucide-react";
 
 const RegisterPage = async () => {
   const session = await getSession();
   if (session) redirect("/feed");
 
   return (
-    <div className="min-h-screen flex items-center justify-center text-foreground bg-background">
+    <div className="min-h-screen w-full flex items-center justify-center text-foreground bg-background">
       <div className="absolute max-md:hidden -left-20 right-0 -top-24 h-100">
         <Image
           src={"https://static.cdninstagram.com/rsrc.php/yC/r/JlaY6JCPfe-.avif"}
@@ -17,6 +18,15 @@ const RegisterPage = async () => {
         />
       </div>
       <div className="w-full max-w-2xl px-6 z-10 md:mt-20">
+        <div className="flex flex-col items-center">
+          <div className="bg-black dark:bg-white p-3 rounded-2xl mb-4">
+            <AtSign
+              size={40}
+              className="text-white dark:text-black"
+              strokeWidth={2.5}
+            />
+          </div>
+        </div>
         <div className="text-center mb-6">
           <h1 className="text-3xl font-bold">Create Threads account</h1>
           <p className="text-neutral-400 mt-2">Join the conversation</p>
